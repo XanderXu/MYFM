31a61b1d4300e5461ebc186cda1dfe6c65c53ace
xuhf <xuhf@bairuitech.com>
2017-01-07 15:08:18 +0800

    创建项目

---
+++ MYFM/MYFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYFM
+//
+//  Created by Mac on 17/1/7.
+//  Copyright © 2017年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
